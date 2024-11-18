75070e0aae1f77da17d9413a6e849c55d1122e66
dsh232 <nifitca.lex@gmail.com>
2024-11-18 05:33:15 +0400

    a

--- infa 3/infa 3.cpp
+++ infa 3/infa 3.cpp
@@ -5,24 +5,23 @@
 
 using namespace std;
 
-class Sudoku {
+class ArrayManager {
 private:
     string** board;
 
 public:
-    Sudoku() {
-        // Создание двумерного динамического массива строк
-        board = new string * [4];
-        for (int i = 0; i < 4; ++i) {
-            board[i] = new string[4];
-            for (int j = 0; j < 4; ++j) {
+    ArrayManager(int size) {
+
+        board = new string * [size];
+        for (int i = 0; i < size; ++i) {
+            board[i] = new string[size];
+            for (int j = 0; j < size; ++j) {
                 board[i][j] = "?"; // Инициализация пустыми ячейками
             }
         }
     }
 
-    ~Sudoku() {
-        // Освобождение памяти
+    ~ArrayManager() {
         for (int i = 0; i < 4; ++i) {
             delete[] board[i];
         }
@@ -47,28 +46,90 @@ public:
                     cout << "uh-huh.\nenter 1 or 2 or 3 or 4 or ? and just normal size of line please (it's 4)." << endl;    //if censored
                     uhHuhs++;
                 }
+                if (uhHuhs == 5) {
+                    cout << endl << "Please, enter your matrix more carefully. Thank you. " << endl << endl;                 
+                }
                 if (uhHuhs > 10) {
                     cout << endl << "don't play with me." << endl;
-                                                                                                                                                                                        /*cout << "   __\n| |\n|_|__\n  | |\n__| |" << endl;*/
+                                                                                                                                                                                            /*cout << "   __\n| |\n|_|__\n  | |\n__| |" << endl;*/
                     exit(1);
                 }
-
             }
-
         }
 
     }
 
     void printBoard() const {
+        cout << "Current board:\n";
         for (int i = 0; i < 4; ++i) {
             for (int j = 0; j < 4; ++j) {
-                cout << board[i][j] << " "; // Выводим текущее состояние доски
+                cout << board[i][j] << " "; 
             }
             cout << endl;
         }
     }
 
+    string** getBoard() const {     //pointer on board
+        return board;
+    }
+
+private:
+    bool isValidChar(char c) const {
+        const vector<char> validChars = { '1', '2', '3', '4', '?' };
+        return find(validChars.begin(), validChars.end(), c) != validChars.end();
+    }
+
+    bool isMyManOkay(const string& text) const {
+        if (text.size() != 4) return false; // Проверяем, что длина строки равна 4
+        for (char c : text) {
+            if (!isValidChar(c)) {
+                return false; // Если хотя бы один символ недопустимый, возвращаем false
+            }
+        }
+        return true;
+    }
+};
+
+class Sudoku {
+private:
+    ArrayManager* arrayManager;
+
+public:
+    Sudoku() {
+        arrayManager = new ArrayManager(4); // Создаем объект ArrayManager для управления массивом
+    }
+
+    ~Sudoku() {
+        delete arrayManager; // Освобождение памяти
+    }
+
+    bool solve() {
+        int row, col;
+        string** board = arrayManager->getBoard();  //calling method from arrayManager
+
+        if (!findEmptyLocation(row, col, board)) {
+            return true; // Решение найдено
+        }
+
+        for (int num = 1; num <= 4; num++) {
+            string strNum = to_string(num); // Преобразуем число в строку
+            if (isValid(row, col, strNum, board)) {
+                board[row][col] = strNum; // Вставляем число
+
+                if (solve()) {
+                    return true;
+                }
+
+                board[row][col] = "?"; // Обозначаем как пустую ячейку
+            }
+        }
+
+        return false; // Нет решения
+    }
+
     bool isValidSolution() const {
+        string** board = arrayManager->getBoard();
+
         // Проверка строк
         for (int i = 0; i < 4; ++i) {
             vector<bool> seen(5, false); // Индексы от 1 до 4
@@ -112,67 +173,28 @@ public:
         return true;
     }
 
-    bool solve() {
-        int row, col;
+    void inputBoard() {
+        arrayManager->inputBoard(); // Ввод данных от пользователя
+    }
 
-        if (!findEmptyLocation(row, col)) {
-            return true; // Решение найдено
-        }
-
-        for (int num = 1; num <= 4; num++) {
-            string strNum = to_string(num); // Преобразуем число в строку
-            if (isValid(row, col, strNum)) {
-                board[row][col] = strNum; // Вставляем число
-
-                if (solve()) {
-                    return true;
-                }
-
-                board[row][col] = "?"; // Обозначаем как пустую ячейку
-            }
-        }
-
-        return false; // Нет решения
+    void printBoard() const {
+        arrayManager->printBoard(); // Вывод текущего состояния доски
     }
 
 private:
-    bool isValidChar(char c) const {
-        const vector<char> validChars = { '1', '2', '3', '4', '?' };
-        return find(validChars.begin(), validChars.end(), c) != validChars.end();
-    }
-
-    bool isMyManOkay(const string& text) const {
-        if (text.size() != 4) return false; // Проверяем, что длина строки равна 4
-        for (char c : text) {
-            if (!isValidChar(c)) {
-                return false; // Если хотя бы один символ недопустимый, возвращаем false
-            }
-        }
-        return true;
-    }
-
-    bool isValid(int row, int col, const string& num) const {
-        // Проверка на дублирование в строке
+    bool isValid(int row, int col, const string& num, string** board) const {
         for (int x = 0; x < 4; x++) {
-            if (board[row][x] == num) {
-                return false; // Дублирование в строке
+            if (board[row][x] == num || board[x][col] == num) {
+                return false; // Проверка на дублирование в строке и столбце
             }
         }
 
-        // Проверка на дублирование в столбце
-        for (int x = 0; x < 4; x++) {
-            if (board[x][col] == num) {
-                return false; // Дублирование в столбце
-            }
-        }
-
-        // Проверка на дублирование в квадрате 2x2
         int startRow = row - row % 2;
         int startCol = col - col % 2;
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
                 if (board[i + startRow][j + startCol] == num) {
-                    return false; // Дублирование в квадрате
+                    return false; // Проверка на дублирование в квадрате
                 }
             }
         }
@@ -180,7 +202,7 @@ private:
         return true;
     }
 
-    bool findEmptyLocation(int& row, int& col) const {
+    bool findEmptyLocation(int& row, int& col, string** board) const {
         for (row = 0; row < 4; row++) {
             for (col = 0; col < 4; col++) {
                 if (board[row][col] == "?") { // Пустая ячейка обозначается как "?"
